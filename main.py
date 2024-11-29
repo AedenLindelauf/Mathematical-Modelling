a0372690f0712864e7cb0f376446582415d4d8b6
AedenLindelauf <aeden.lindelauf@gmail.com>
2024-11-29 14:12:31 +0100

    Created a GUI.

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 from tree import Tree
 from node import *
 from shunting_yard import *
+from GUI import GUI
 
 if __name__ == "__main__":
-    
-    # post_fixer_1 = Post_Fixer("2 ^ ( 2 + x ^ 2 * y )")
-    # test_tree = create_tree(post_fixer_1.postfix_notation)
-    # print(test_tree)
-
-    expr = "( 2 - 1 ) ^ a * ( 0 ^ 5 ) * ( 1 * x + y + 0 ) ^ 2 * 1 * ( ( z / 1 ) ^ 1 )"
-
-    post_fixer_2 = Post_Fixer(expr)
-    test_tree = create_tree(post_fixer_2.postfix_notation)
-    test_tree.simplify()
-    print(f"Input:\t {expr}\nOutput:\t {test_tree}")
+    gui = GUI()
