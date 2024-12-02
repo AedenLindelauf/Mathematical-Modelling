1a089a82cc4fb99c0affeb0919a3225101742053
Giel <72444578+Maety@users.noreply.github.com>
2024-12-02 14:50:41 +0100

    Shunting Yard Updates

--- main.py
+++ main.py
@@ -4,13 +4,6 @@ from shunting_yard import *
 
 if __name__ == "__main__":
     
-    # post_fixer_1 = Post_Fixer("2 ^ ( 2 + x ^ 2 * y )")
-    # test_tree = create_tree(post_fixer_1.postfix_notation)
-    # print(test_tree)
-
-    expr = "2 * ( 1 * x + y + 0 ) ^ 2 * 1 * ( ( z / 1 ) ^ 1 )"
-
-    post_fixer_2 = Post_Fixer(expr)
-    test_tree = create_tree(post_fixer_2.postfix_notation)
-    test_tree.simplify()
-    print(f"Input:\t {expr}\nOutput:\t {test_tree}")
+    post_fixer_1 = Post_Fixer("2 ^ ( 2 + x ^ 2 * y )")
+    test_tree = create_tree(post_fixer_1.postfix_notation)
+    print(test_tree)
