f70061666ef9598078f66ba35cf61c6510fd71a1
Giel <72444578+Maety@users.noreply.github.com>
2024-12-05 14:07:23 +0100

    Tussen Commit

--- test_verification.py
+++ test_verification.py
@@ -6,25 +6,30 @@ operator_pattern_creator.extend([f"\\{operator}" for operator in OPERATORS])
 operator_pattern_creator.append("]{1}$")
 operator_pattern_str = "".join(operator_pattern_creator)
 OPERATOR_PATTERN = re.compile(operator_pattern_str)
-print(OPERATOR_PATTERN.match("-"))
-
-
-
-
 NUMBER_PATTERN = re.compile(r"(^[0-9]+(\.[0-9])?$)")
+INVALID_ChARACTERS_PATTERN = re.compile(r'[^a-zA-Z0-9\+\-\*/\^()\s]')
 
 
 def is_numerical_value(token : str) -> bool:
     return bool(NUMBER_PATTERN.match(token))
 
 def is_operator(token : str) -> bool:
-    pass
+    return bool(OPERATOR_PATTERN.match(token))
+
+def constists_of_valid_characters(expression : str, return_invalid_characters : bool = False) -> bool | tuple[bool, list[str]]:
+    invalid_characters_used = INVALID_ChARACTERS_PATTERN.findall(expression)
+
+    if not invalid_characters_used:
+        return True
+    
+    return False, invalid_characters_used if return_invalid_characters else False
+
+
 if __name__ == "__main__":
-    is_operator("+")
-    # while True:
-    #     test_string = input("test string: ")
-    #     if is_numerical_value(test_string):
-    #         print("hey")
-    #     else:
-    #         print("not hey")
-    #         break
+    while True:
+        test_string = input("test string: ")
+        if constists_of_valid_caracters(test_string):
+            print("hey")
+        else:
+            print("not hey")
+            break
