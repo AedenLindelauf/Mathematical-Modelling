0d557e6c51eedfa08db19a9ff6077c054c517366
Giel <72444578+Maety@users.noreply.github.com>
2024-12-13 00:33:56 +0100

    expression Class

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from verify_expression import *
 from tree import Tree
 from node import *
-from shunting_yard import *
+from expression import *
 import verification
 
 if __name__ == "__main__":
-    print(is_operator("1"))
+    e = Expression("(x+1)/2")
+    print(e.node_stack)
+    
+    
