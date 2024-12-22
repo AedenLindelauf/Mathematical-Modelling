553e2b92e3057a4b380694d043dc4c91181f2810
AedenLindelauf <aeden.lindelauf@gmail.com>
2024-12-22 16:22:14 +0100

    For now, we also check whether the input is the same which is not the case but we can manually check if it is.

--- TESTING.py
+++ TESTING.py
@@ -3,45 +3,62 @@ from colorama import Fore, Back, Style, init
 from os import system
 import pandas
 
-init(autoreset=True)
 
-test_cases_simplification_df = pandas.read_excel("TESTCASES_SIMPLIFICATION.xlsx", 
-                                                 header=0, dtype={'INPUT': str, 'OUTPUT': str})
+def TEST(simplify: bool):
 
-test_cases_simplification = []
+    if simplify:
+        test_cases_df = pandas.read_excel("TESTCASES_SIMPLIFICATION.xlsx",
+                                          header=0, dtype={'INPUT': str, 'OUTPUT': str})
+    else:
+        test_cases_df = pandas.read_excel("TESTCASES_DIFFERENTIATION.xlsx", 
+                                          header=0, dtype={'INPUT': str, 'OUTPUT': str})
 
-system("cls")
-print("====== TESTING SIMPLIFICATION FUNCTIONALITY ======\n")
+    if simplify:
+        print("====== TESTING SIMPLIFICATION FUNCTIONALITY ======\n")
+    else:
+        print("====== TESTING DIFFERENTIATION FUNCTIONALITY =====\n")
 
-prev_failed = False
-for index in range(len(test_cases_simplification_df)):
-    try:
-        test_case = test_cases_simplification_df['INPUT'].iloc[index]
-        answer = test_cases_simplification_df['OUTPUT'].iloc[index]
+    prev_failed = False
+    for index in range(len(test_cases_df)):
+        
+        try:
+            test_case = test_cases_df['INPUT'].iloc[index]
+            answer = test_cases_df['OUTPUT'].iloc[index]
 
-        input_expr  = Expression(test_case)
-        answer      = Expression(answer)
-        input_text  = input_expr.__str__()
-        input_expr.simplify()
+            input_expr  = Expression(test_case)
+            input_as_text = input_expr.__str__()
+            answer      = Expression(answer)
 
-        if (test_case != input_expr.__str__()) and input_expr.tree.root.compare(answer.tree.root):
-            print(Fore.GREEN + f'Passed test {index + 1}')
-            prev_failed = False
-        else:
+            if simplify:
+                input_expr.simplify()
+            else:
+                raise NotImplementedError("Differentiation not yet implemented...")
+
+            if ( test_case == input_as_text ) and input_expr.tree.root.compare(answer.tree.root):
+                print(Fore.GREEN + f'Passed test {index + 1}')
+                prev_failed = False
+            else:
+                if not prev_failed:
+                    print("--------------------------------------------------")
+                print(Fore.RED + Style.BRIGHT + f'Failed test {index+1}')
+                print(f"Test case:\t\t{test_case}")
+                print(f"Input Expression:\t{input_as_text}")
+                print(f"Expected output:\t{answer}")
+                print(f"Given output:\t\t{input_expr}")
+                print("--------------------------------------------------")
+                prev_failed = True
+        except:
             if not prev_failed:
                 print("--------------------------------------------------")
             print(Fore.RED + Style.BRIGHT + f'Failed test {index+1}')
-            print(f"Test case:\t\t{test_case}")
-            print(f"Expected output:\t{answer}")
-            print(f"Given output:\t\t{input_expr}")
+            print("ERROR OCCURED")
             print("--------------------------------------------------")
             prev_failed = True
-    except:
-        if not prev_failed:
-            print("--------------------------------------------------")
-        print(Fore.RED + Style.BRIGHT + f'Failed test {index+1}')
-        print("ERROR OCCURED")
-        print("--------------------------------------------------")
-        prev_failed = True
 
-print("\n================= END OF TESTING =================\n\n")
+    print("\n================= END OF TESTING =================\n\n")
+
+if __name__ == "__main__":
+    init(autoreset=True)
+    system("cls")
+
+    TEST(True)
